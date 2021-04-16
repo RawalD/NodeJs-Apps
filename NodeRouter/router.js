b94e98c63c914d362eaa2628a18547841cded3d0
rawald <diprawal@hotmail.com>
2021-04-16 11:23:59 +0200

    NodeJs routing with express

--- NodeRouter/router.js
+++ NodeRouter/router.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const route = express.Router()
+
 const accounts = require('./database')
+
+
 //GET
 route.get('/accounts', (req,res)=>{
     res.json({userData: accounts})
+})
+
+//POST
+route.post('/accounts',(req,res)=>{
+    const newAcc = req.body
+    accounts.push(newAcc)
+    res.json(accounts)
 })
 
 module.exports = route
--- NodeRouter/server.js
+++ NodeRouter/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = require('./router')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.use('/api',router)
 
