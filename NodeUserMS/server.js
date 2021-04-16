fe38fac696329400650a45c346cee69f7024d5e7
rawald <diprawal@hotmail.com>
2021-04-16 14:27:19 +0200

    templates added

--- NodeUserMS/server.js
+++ NodeUserMS/server.js
@@ -1,11 +1,31 @@
 const express = require('express')
-
-const port = process.env.PORT || 3000
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const path = require('path')
 
 const app = express()
 
+dotenv.config({path:'config.env'})
+const port = process.env.PORT || 8080
+
+//Logger
+app.use(morgan('tiny'))
+
+//Request ot bodyParser
+app.use(bodyParser.urlencoded({extended: true}))
+
+//set view engine
+app.set('view engine', 'ejs')
+app.set('views', path.resolve(__dirname,'views/templates'))
+
+//add assets
+app.use('/css',express.static(path.resolve(__dirname,'assets/css')))
+app.use('/js', express.static(path.resolve(__dirname,'assets/js')))
+app.use('/img',express.static(path.resolve(__dirname,'assets/img')))
+
 app.get('/', (req,res)=>{
-    res.send("User Management System")
+    res.render('index')
 })
 
 app.listen(port,()=> console.log(`Active on port ${port}`))
