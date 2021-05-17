6570fd03612ae07a724c20d7cfab234294fb0b5b
rawald <diprawal@hotmail.com>
2021-05-17 13:46:46 +0200

    handlebars templating

--- NodeCMS/app.js
+++ NodeCMS/app.js
@@ -1,5 +1,21 @@
 const express = require('express')
+const path = require('path')
+const expresshbs = require('express-handlebars')
 
 const app = express()
+
+//Middleware
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.engine('handlebars', expresshbs({defaultLayout: 'home'}))
+app.set('view engine', 'handlebars')
+
+app.get('/',(req,res)=>{
+    res.render('home/index')
+})
+
+app.get('/about',(req,res)=>{
+    res.render('home/about')
+})
 
 app.listen(3000, ()=>console.log(`Active on port 3000`))
